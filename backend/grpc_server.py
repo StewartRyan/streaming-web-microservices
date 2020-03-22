5c8b648f6fbb597c5160c0c9e027d0a3d91ba9ef
Ryan Stewart <ryan.stewart@mycit.ie>
2020-03-22 02:56:55 +0000

    Renamed appropriately

--- backend/grpc_server.py
+++ backend/grpc_server.py
@@ -4,23 +4,23 @@ import csv
 
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
+import tweets_pb2
+import tweets_pb2_grpc
 
 import time
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
-    def SayHello(self, request, context):        
+class MessageSender(tweets_pb2_grpc.MessageSenderServicer):
+    def SendMessage(self, request, context):        
         with open('tweets.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 time.sleep(2)            
-                yield helloworld_pb2.HelloReply(message=str(row))
+                yield tweets_pb2.RequestMessage(message=str(row))
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    tweets_pb2_grpc.add_MessageSenderServicer_to_server(MessageSender(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
@@ -29,5 +29,3 @@ def serve():
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
-
-# python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. helloworld.proto
--- streamclient/grpc_client.py
+++ streamclient/grpc_client.py
@@ -3,15 +3,15 @@ import logging
 import redis
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
+import tweets_pb2
+import tweets_pb2_grpc
 conn = redis.Redis('redis-service')
 
 def run():
     with grpc.insecure_channel('grpc-server:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        stub = tweets_pb2_grpc.MessageSenderStub(channel)
         total_length = 0        
-        for response in stub.SayHello(helloworld_pb2.HelloRequest()):
+        for response in stub.SendMessage(tweets_pb2.ResponseMessage()):
             response_array = eval(response.message)         
             total_length += len(response.message)
             conn.hmset(response_array[1], {"tweet" : str(response_array), "data": total_length})        
